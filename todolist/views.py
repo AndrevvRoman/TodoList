bacbd910219c8703f320172acb880f1721277b22
AndrevvRoman <andrey_romanof@mail.ru>
2021-02-06 13:20:38 +0700

    Отладил вход разных пользователей Отладил отображение списка дел каждого пользователя

--- todolist/views.py
+++ todolist/views.py
@@ -1,34 +1,32 @@
-from django.shortcuts import render, redirect #для отображения и редиректа берем необходимые классы
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
-from .models import Todo #не забываем наши модели
-
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
+
+from .models import Todo 
  
 def redirect_view(request):
-	return redirect("/registrate") # редирект с главной на категории
+	return redirect("/sign_in")
 
 def todo(request):
-    todos = TodoList.objects.all() #запрашиваем все объекты todo через менеджер объектов
-    categories = Category.objects.all() #так же получаем все Категории
+    todos = Todo.objects.filter(owner=request.user)
 
-    if request.method == "POST": #проверяем то что метод именно POST
-        if "Add" in request.POST: #проверяем метод добавления todo
-            title = request.POST["description"] #сам текст
-            date = str(request.POST["date"]) #дата, до которой должно быть закончено дело
-            category = request.POST["category_select"] #категория, которой может выбрать или создать пользователь.
-            content = title + " -- " + date + " " + category # полный склеенный контент
-            Todo = TodoList(title=title, content=content, due_date=date, category=Category.objects.get(name=category))
-            Todo.save() # сохранение нашего дела
-            return redirect("/todo") # перегрузка страницы (ну вот так у нас будет устроено очищение формы)
+    if request.method == "POST":
+        if "Add" in request.POST:
+            title = request.POST["description"]
+            date = str(request.POST["date"])
+            content = title + " -- " + date + " " # полный склеенный контент
+            newTodo = Todo(title=title, content=content, due_date=date, owner=request.user)
+            newTodo.save()
+            return redirect("/todo")
         if "Delete" in request.POST: #если пользователь собирается удалить одно дело
             checkedlist = request.POST.getlist('checkedbox') # берем список выделенные дел, которые мы собираемся удалить
-            for i in range(len(checkedlist)): #мне почему-то не нравится эта конструкция
-                todo = TodoList.objects.filter(id=int(checkedlist[i]))
+            for i in range(len(checkedlist)):
+                todo = Todo.objects.filter(id=int(checkedlist[i]))
                 todo.delete() #удаление дела
-    return render(request, "todo.html", {"todos": todos, "categories": categories})
+    return render(request, "todo.html", {"todos": todos})
 
 def sign_in(request):
     if request.method == "POST":
@@ -38,12 +36,11 @@ def sign_in(request):
             user = authenticate(request, username=enteredUsername, password=enteredPass)
             if user is not None:
                 login(request,user)
-                return render(request, "todo.html", {"todos" : Todo.objects.filter(owner=user)})
-    return render(request, "sign_in.html", {"hasError" : True})
-
-@login_required
-def index(request):
-    return render(request,"category.html")
+                todos = Todo.objects.filter(owner=request.user)
+                return redirect('/todo')
+                # return redirect(reverse('todo.html',{"todos" : Todo.objects.filter(owner=user)}))
+                # return render(request, "todo.html", {"todos" : Todo.objects.filter(owner=user)})
+    return render(request, "sign_in.html", {"hasError" : False})
 
 def registrate(request):
     context = {}
